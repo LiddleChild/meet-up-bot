43d920d3830f0331072041a8e995f548624c26ff
bosstnp <boss.32560@gmail.com>
2023-07-02 13:01:07 +0700

    feat: add cors

--- src/app/app.web.ts
+++ src/app/app.web.ts
@@ -2,10 +2,12 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import * as apiController from "../controllers/controller.api";
 
 const app = express();
 
+app.use(cors());
 app.get("/event/:eventId", apiController.getEvent);
 
 export default app;
